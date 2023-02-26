c7cd748ad455a6ef1e2cfa72154e249f7a82d8cb
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-02-26 13:01:18 +0000

    portfolio section added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+const myNav = document.getElementById('navbar');
+window.onscroll = function() {
+  if (document.body.scrollTop >= 1) {
+    myNav.classList.remove('nav-coloured');
+  } else {
+    myNav.classList.add('nav-coloured');
   }
-}
+};
+
+
+
