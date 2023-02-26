622e01c25fd324280b17d2aa1898eb5754be2cfa
Jason Blackburn <113805105+JBlackburn94@users.noreply.github.com>
2023-02-26 17:20:57 +0000

    responsive navbar added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myNav = document.getElementById('navbar');
+const myNav = document.querySelector('.navbar');
 window.onscroll = function() {
   if (document.body.scrollTop >= 1) {
     myNav.classList.remove('nav-coloured');
@@ -6,6 +6,24 @@ window.onscroll = function() {
     myNav.classList.add('nav-coloured');
   }
 };
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+const navLink = document.querySelectorAll('.nav-link');
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
 
 
 
